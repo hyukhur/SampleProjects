42c3e2dcfe607e240c173e6b634f179248afb6f3
Hyuk Hur <hyukhur@gmail.com>
2016-07-04 22:06:43 +0900

    Remove all objective-c codes

--- SampleProject/SampleProject/AppDelegate.swift
+++ SampleProject/SampleProject/AppDelegate.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2016년 hyukhur. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]? = [:]) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+}
